e560f4580213ecb5b4c975867b73156f59daebfe
“sonmezsoftware” <sonmezsoftware@gmail.com>
2024-01-19 11:01:25 +0300

    replica set güncellemesi yapıldı.

--- kurulum.md
+++ kurulum.md
@@ -1,9 +1,3 @@
 ```shell
-docker exec -it mongodb1 mongo
-
-rsconf = { _id : "rs0",members: [{"_id": 0,"host": "wn-mongodb1:27017","priority": 4},{"_id": 1,"host": "wn-mongodb2:27018","priority": 2},{"_id": 2,"host": "wn-mongodb3:27019","priority": 1 }]}
-
-rs.initiate(rsconf); 
-rs.conf() 
-rs.secondaryOk() 
+docker exec -it mongodb1 mongosh /home/mongo-setup.js
 ```
--- mongo-setup/mongo-setup.js
+++ mongo-setup/mongo-setup.js
@@ -1,22 +1,3 @@
-rsconf = {
-    _id : "rs0",
-    members: [
-        {
-            "_id": 0,
-            "host": "mongodb1:27017",
-            "priority": 3
-        },
-        {
-            "_id": 1,
-            "host": "mongodb2:27017",
-            "priority": 2
-        },
-        {
-            "_id": 2,
-            "host": "mongodb3:27017",
-            "priority": 1
-        }
-    ]
-}
+rsconf = {_id : "rs0", members: [{"_id": 0, "host": "mongodb1:27017", "priority": 3}, {"_id": 1, "host": "mongodb2:27018", "priority": 2}, {"_id": 2, "host": "mongodb3:27019", "priority": 1}]}
 
 rs.initiate(rsconf);
